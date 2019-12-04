4200750bb7cf8671ec1b9b5e29d584d4bc50b94b
bob-crutchley <bob.crutchley96@gmail.com>
2019-12-04 08:23:08 +0000

    changes

--- automation/check_structure.py
+++ automation/check_structure.py
@@ -1,17 +1,18 @@
+#! /usr/bin/env python
 import yaml
 
 with open('./automation/structure.yaml') as file:
     test = yaml.load(file, Loader=yaml.FullLoader)
 
-def get_type(child):
-    if 'type' not in child:
-        raise Exception('child does not have "type" field set: ' + str(child))
+def get_property(item, prop):
+    if 'type' not in item:
+        raise Exception('child does not have "{0}" field set: {1}'.format(prop,str(item)))
     else:
-        return child['type']
+        return item[prop]
 
 def iterate(children, depth=0):
     for child in children:
-        print(get_type(child))
+        print(get_property(child, 'type'))
         if 'children' in child:
             iterate(child['children'], depth + 1)
 
