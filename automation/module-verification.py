dbd1417908e930d898f0820076ea5ffc86043091
bob-crutchley <bob.crutchley96@gmail.com>
2019-12-12 19:03:58 +0000

    working on checking heading order

--- automation/module-verification.py
+++ automation/module-verification.py
@@ -10,6 +10,8 @@ with open('./automation/module.yaml') as module_yaml:
 
 
 def get_modules():
+    # TODO REMOVE
+    return ["/tmp/module.md"]
     modules = []
     state = get_state()
     for topic in state['topics']:
@@ -37,13 +39,51 @@ def get_matches(lines, spec):
             matched.append(match)
     return matched
 
+def get_non_code_block_lines(lines):
+    non_code_block_lines = []
+    code_block = False
+    for line in lines:
+        if line.startswith("```"):
+            code_block = not code_block
+        if not code_block:
+            non_code_block_lines.append(line)
+    return non_code_block_lines
+
+def match_spec(line, specs):
+    for index, spec in enumerate(specs):
+        pattern = re.compile(spec['heading'])
+        match = pattern.search(line)
+        if match:
+            return True, index;
+    return False, 0;
+
+def wrong_order_error(order, module, specs):
+    print("File: {0}\nDescription: The headings are in the wrong order.".format(module))
+    for index, spec in enumerate(specs):
+        if index != order[index]:
+            if index > order[index]:
+                if len(
+                print("Heading {0} should be after the {1} and before the {2}".format(spec[index]['name'], spec[index-1]['name'],spec[index+1]['name'] ))
+
+
 def get_errors():
     errors = []
     for module in get_modules():
         with open(module) as readme_file:
             readme_lines = readme_file.readlines()
+        lines = get_non_code_block_lines(readme_lines)
 
-        for spec in specs:
+        order = []
+
+        for line in lines:
+            matched, spec_index = match_spec(line, specs)
+            if matched:
+                order.append(spec_index)
+
+        if sorted(order) != order:
+            wrong_order_error(order, module, specs)
+
+        for index, spec in enumerate(specs):
             matched = get_matches(readme_lines, spec)
             count = get_count_for_spec(spec)
 
