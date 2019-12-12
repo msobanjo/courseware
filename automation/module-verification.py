0caec21428a226d59dcaa8cd00dc717cb958f832
bob-crutchley <bob.crutchley96@gmail.com>
2019-12-12 18:14:15 +0000

    refactored a bit

--- automation/module-verification.py
+++ automation/module-verification.py
@@ -9,41 +9,51 @@ with open('./automation/module.yaml') as module_yaml:
     specs = yaml.load(module_yaml, Loader=yaml.FullLoader)
 
 
-modules = []
-state = get_state()
-for topic in state['topics']:
-    for module in topic['modules']:
-        modules.append(module['gitUri'] + '/README.md')
+def get_modules():
+    modules = []
+    state = get_state()
+    for topic in state['topics']:
+        for module in topic['modules']:
+            modules.append(module['gitUri'] + '/README.md')
+    return modules
 
-errors = []
-for module in modules:
-    print(module)
-    with open(module) as readme_file:
-        readme_lines = readme_file.readlines()
+def get_count_for_spec(spec):
+    if 'count' in spec:
+        return spec['count']
+    else:
+        return 1
 
-    for spec in specs:
-        print(spec['heading'])
-        matched = []
-        pattern = re.compile(spec['heading'])
-        code_block = False
-        for line in readme_lines:
-            if line.startswith("```"):
-                code_block = not code_block
 
-            match = pattern.search(line)
-            if match and not code_block:
-                print(line)
-                matched.append(match)
+def get_matches(lines, spec):
+    matched = []
+    pattern = re.compile(spec['heading'])
+    code_block = False
+    for line in lines:
+        if line.startswith("```"):
+            code_block = not code_block
 
-        if 'count' in spec:
-            count = spec['count']
-        else:
-            count = 1
+        match = pattern.search(line)
+        if match and not code_block:
+            matched.append(match)
+    return matched
 
-        if len(matched) < count:
-            errors.append("File: {2}\nDescription: There needs to be at least {0} {1}\n".format(count, spec['description'], module))
-        elif len(matched) > count:
-            errors.append("File: {2}\nDescription: There can be no more than {0} {1}\n".format(count, spec['description'], module))
+def get_errors():
+    errors = []
+    for module in get_modules():
+        with open(module) as readme_file:
+            readme_lines = readme_file.readlines()
+
+        for spec in specs:
+            matched = get_matches(readme_lines, spec)
+            count = get_count_for_spec(spec)
+
+            if len(matched) < count:
+                errors.append("File: {2}\nDescription: There needs to be at least {0} {1}\n".format(count, spec['description'], module))
+            elif len(matched) > count:
+                errors.append("File: {2}\nDescription: There can be no more than {0} {1}\n".format(count, spec['description'], module))
+    return errors
+
+errors = get_errors()
 
 if len(errors) > 0:
     rows, columns = os.popen('stty size', 'r').read().split()
