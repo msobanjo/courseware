d370780e70e70ad46919c75fefe4a11c7ef88344
bob-crutchley <bob.crutchley96@gmail.com>
2019-12-12 17:21:36 +0000

    basic module checks working

--- automation/module-verification.py
+++ automation/module-verification.py
@@ -2,37 +2,44 @@
 
 import yaml
 import re
-
+import os
+from functions import get_state
 
 with open('./automation/module.yaml') as module_yaml:
     specs = yaml.load(module_yaml, Loader=yaml.FullLoader)
 
-file = '/tmp/module.md'
 
-with open(file) as readme_file:
-    readme_lines = readme_file.readlines()
+modules = []
+state = get_state()
+for topic in state['topics']:
+    for module in topic['modules']:
+        modules.append(module['gitUri'] + '/README.md')
 
+errors = []
+for module in modules:
+    with open(module) as readme_file:
+        readme_lines = readme_file.readlines()
 
-errors = ""
+    for spec in specs:
+        matched = []
+        pattern = re.compile(spec['heading'])
+        for line in readme_lines:
+            match = pattern.search(line)
+            if match:
+                matched.append(match)
 
-for spec in specs:
-    print("spec: {0}".format(spec))
-    matched = []
-    pattern = re.compile(spec['heading'])
-    for line in readme_lines:
-        match = pattern.search(line)
-        if match:
-            matched.append(match)
+        if 'count' in spec:
+            count = spec['count']
+        else:
+            count = 1
 
-    if 'count' in spec:
-        count = spec['count']
-    else:
-        count = 1
+        if len(matched) < count:
+            errors.append("There needs to be at least {0} {1} in the file: {2}\n".format(count, spec['description'], module))
+        elif len(matched) > count:
+            errors.append("There can be no more than {0} {1} in the file: {2}".format(count, spec['description'], module))
 
-    if len(matched) < count:
-        errors = errors + "There needs to be at least {0} {1} in the file: {2}\n".format(count, spec['description'], file)
-    elif len(matched) > count:
-        errors = errors + "There can be no more than {0} {1} in the file: {2}".format(count, spec['description'], file)
+if len(errors) > 0:
+    rows, columns = os.popen('stty size', 'r').read().split()
+    seperator = '\n' + ('=' * int(columns)) + '\n'
+    raise Exception(seperator + seperator.join(errors) + '\n\nError count: ' + str(len(errors)))
 
-if errors:
-    raise Exception(errors)
